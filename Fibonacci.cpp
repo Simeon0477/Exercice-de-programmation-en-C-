507c7ddcf5921cab4145ad4cd0261906ac03de42
Simeon0477 <nouhoumsimeon01@gmail.com>
2024-10-14 14:46:33 +0100

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 
-unsigned int nbr, elt = 0;
+//Déclaration des variables
+unsigned int nbr = 0;
 unsigned int fibonacci(unsigned int n);
 
 int main(int argc, char** args){
-	//Entrée de la valeur
+	//Entrée des valeurs par l'utilisateur
 	std::cout <<"Entrer la valeur du dernier element de la suite que vous desirez"<<std::endl;
 	std::cin  >> nbr;
 	
-	//Calcul de la suite
-	std::cout <<"La suite de Fibonacci de "<< nbr <<" est:"<<std::endl;
+	//Début de l'exécution
+	std::cout <<"La suite de Fibonacci pour la valeur "<< nbr <<" est:"<<std::endl;
 	for(int i=0;i<=nbr;i++){
-		elt = fibonacci(i);
 		//Affichage des éléments de la suite
-		std::cout<<elt<<std::endl;
+		std::cout<<"F("<<i<<") = "<<fibonacci(i)<<std::endl;
 	}
 	return 0;
 }
@@ -21,10 +21,10 @@ int main(int argc, char** args){
 unsigned int fibonacci(unsigned int n){
 	
 	if(n == 0 || n == 1)
-		//Retourner 1 pour les valeurs 0 et 1
+		//Retourner 1 pour les valeurs 1 et 0
 		return 1;
 	else
-		//Calcul de la valeur à retourner pour n > 1
+		//Valeur retourner dans le cas de n > 1
 		return fibonacci(n-1) + fibonacci(n-2);
 
 }
