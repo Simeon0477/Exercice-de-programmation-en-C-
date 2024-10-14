f213c87565896bfd25b3d558b1eacdcf7c600b57
Simeon0477 <nouhoumsimeon01@gmail.com>
2024-10-14 14:47:25 +0100

    Update Mandelbrot.cpp

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 double part_reel, part_img;
-const int iteration = 1000;
+const int DIM = 1000;
 
 int mandelbrot(double reel,double img);
 
@@ -12,11 +12,12 @@ int main(int argc, char** args){
 	std::cout<<"Entrez la valeur de la partie imaginaire du nombre"<<std::endl;
 	std::cin >> part_img;
 	
+	std::cout<<"\nDébut de l'exécution \n"<<std::endl;
 	//Appel de la fonction
 	int it = mandelbrot(part_reel, part_img);
-	
+
 	//Conclusion
-	if(it < iteration)
+	if(it < DIM)
 		std::cout<<"le nombre n'appartient pas a l'ensemble de Mandelbrot\n"
 				 <<"Il diverge a partir de l'iteration "<<it<<std::endl;	
 	else
@@ -28,19 +29,13 @@ int main(int argc, char** args){
 int mandelbrot(double reel,double img){
 	double z_rl, z_img = 0;
 	int i = 0;
-	
-	//Calcul de la nouvelle valeur de Z
-	double new_z_rl = z_rl*z_rl - z_img*z_img + reel;
-	double new_z_img = 2*z_rl*z_img + img;
-	//Affectation aux variables de base
-	z_rl = new_z_rl;
-	z_img = new_z_img;
-	//Affichage du résultat
-	std::cout<<"Z("<<i<<") = "<<z_rl<<" + "<<z_img<<"i"<<std::endl;
-	//Incrémentation du compteur d'itération
+	//Initialisation
+	std::cout<<"Z(0) = 0"<<std::endl;
 	i += 1;
+
+	double new_z_rl, new_z_img;
 	
-	while((z_rl*z_rl - z_img*z_img + 2*z_rl*z_img) <= 4 && i <= iteration){
+	while((z_rl*z_rl + z_img*z_img) <= 4 && i <= DIM){
 		//Calcul de la nouvelle valeur de Z
 		new_z_rl = z_rl*z_rl - z_img*z_img + reel;
 		new_z_img = 2*z_rl*z_img + img;
